b96566f1a0621ad80d7c8672ea138faf77cd67f4
baidu <baidu@xijiayuntekiMacBook-Pro.local>
2014-12-23 17:48:50 +0800

    Initial Commit

---
+++ 116/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  116
+//
+//  Created by baidu on 14/12/23.
+//  Copyright (c) 2014年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 116/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  116
+//
+//  Created by baidu on 14/12/23.
+//  Copyright (c) 2014年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
